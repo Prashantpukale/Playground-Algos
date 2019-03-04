7bd770ddd11885a0c27ea6d7f1d2ffbb5cd03b7d
Prashant Pukale <prashant.pukale@grabtaxi.com>
2019-03-04 20:54:01 +0530

    Added Tree class and Traversal methods.

---
+++ Algos/Algos/Tree.swift
@@ -0,0 +1,37 @@
+//
+//  Algos.swift
+//  Algos
+//
+//  Created by Prashant Pukale on 04/03/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+class BTree<T> {
+  var root: BTreeNode<T>?
+}
+
+class BTreeNode<T> {
+  let value: T
+  var left: BTreeNode<T>?
+  var right: BTreeNode<T>?
+  init(value: T) {
+    self.value = value
+  }
+
+  func traverseInorder(completion: ((T)->())? ) {
+    left?.traverseInorder(completion: completion)
+    completion?(value)
+    right?.traverseInorder(completion: completion)
+  }
+  func traversePreorder(completion: ((T)->())? ) {
+    completion?(value)
+    left?.traverseInorder(completion: completion)
+    right?.traverseInorder(completion: completion)
+  }
+  func traversePostorder(completion: ((T)->())? ) {
+    left?.traverseInorder(completion: completion)
+    right?.traverseInorder(completion: completion)
+    completion?(value)
+  }
+}
+
