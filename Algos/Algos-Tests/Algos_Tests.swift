b10c4883cad849d0afd32545ac0fe8c501e10689
Prashant.Pukale <prash.p.jc@gmail.com>
2019-03-06 22:06:28 +0530

    LCA function and tests. Plus equatable compliance.

--- Algos/Algos-Tests/Algos_Tests.swift
+++ Algos/Algos-Tests/Algos_Tests.swift
@@ -10,151 +10,218 @@ import XCTest
 @testable import Algos
 
 class Algos_Tests: XCTestCase {
+  
+  private var tree1: BTree<Int> {
+    let rootNode = BTreeNode<Int>(value: 100)
+    let left1 = BTreeNode<Int>(value: 50)
+    let right1 = BTreeNode<Int>(value: 150)
+    rootNode.left = left1
+    rootNode.right = right1
+    let treeNode = BTree<Int>()
+    treeNode.root = rootNode
+    return treeNode
+  }
+  private var tree2: BTree<Int> {
+    let rootNode = BTreeNode<Int>(value: 100)
+    let left1 = BTreeNode<Int>(value: 50)
+    let left2 = BTreeNode<Int>(value: 25)
+    rootNode.left = left1
+    left1.left = left2
+    let treeNode = BTree<Int>()
+    treeNode.root = rootNode
+    return treeNode
+  }
+  private var tree3: BTree<Int> {
+    let rootNode = BTreeNode<Int>(value: 100)
+    let right1 = BTreeNode<Int>(value: 150)
+    let right2 = BTreeNode<Int>(value: 200)
+    rootNode.right = right1
+    right1.right = right2
+    let treeNode = BTree<Int>()
+    treeNode.root = rootNode
+    return treeNode
+  }
+  
+  typealias LCATestDataType = (root:BTree<Int>, node1: BTreeNode<Int>?, node2: BTreeNode<Int>?, lca: BTreeNode<Int>?)
+  
+  private var lcaDataSet1: LCATestDataType  {
+    let rootNode = BTreeNode<Int>(value: 100)
+    let left1 = BTreeNode<Int>(value: 50)
+    let left11 = BTreeNode<Int>(value: 25)
+    let right11 = BTreeNode<Int>(value: 75)
+    let right1 = BTreeNode<Int>(value: 150)
+    let rightLeft1 = BTreeNode<Int>(value: 125)
+    let rightRight1 = BTreeNode<Int>(value: 175)
+    rootNode.left = left1
+    rootNode.right = right1
+    left1.left = left11
+    left1.right = right11
+    right1.left = rightLeft1
+    right1.right = rightRight1
+    let tree = BTree<Int>()
+    tree.root = rootNode
+    return (tree, left11, rightRight1, rootNode)
+  }
+  private var lcaDataSet2: LCATestDataType {
+    let rootNode = BTreeNode<Int>(value: 100)
+    let left1 = BTreeNode<Int>(value: 50)
+    let left11 = BTreeNode<Int>(value: 25)
+    let right11 = BTreeNode<Int>(value: 75)
+    let right1 = BTreeNode<Int>(value: 150)
+    let rightLeft1 = BTreeNode<Int>(value: 125)
+    let rightRight1 = BTreeNode<Int>(value: 175)
+    rootNode.left = left1
+    rootNode.right = right1
+    left1.left = left11
+    left1.right = right11
+    right1.left = rightLeft1
+    right1.right = rightRight1
+    let tree = BTree<Int>()
+    tree.root = rootNode
+    return (tree, left11, right11, left1)
+  }
+  private var lcaDataSet3: LCATestDataType {
+    let rootNode = BTreeNode<Int>(value: 100)
+    let left1 = BTreeNode<Int>(value: 50)
+    let left11 = BTreeNode<Int>(value: 25)
+    let right11 = BTreeNode<Int>(value: 75)
+    let right1 = BTreeNode<Int>(value: 150)
+    let rightLeft1 = BTreeNode<Int>(value: 125)
+    let rightRight1 = BTreeNode<Int>(value: 175)
+    rootNode.left = left1
+    rootNode.right = right1
+    left1.left = left11
+    left1.right = right11
+    right1.left = rightLeft1
+    right1.right = rightRight1
+    let tree = BTree<Int>()
+    tree.root = rootNode
+    return (tree, rightLeft1, rightRight1, right1)
+  }
+  
+  
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testInOrderSimple() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var inOrderArray = [50,100,150]
+    var index=0
+    tree1.root?.traverseInorder() { node in
+      XCTAssertTrue( node.value == inOrderArray[index] )
+      index += 1
+    }
+  }
+  
+  func testInOrderSimpleAllLeft() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var inOrderArray = [25,50,100]
+    var index=0
+    tree2.root?.traverseInorder() { node in
+      XCTAssertTrue( node.value == inOrderArray[index])
+      index += 1
+    }
+  }
+  
+  func testInOrderSimpleAllRight() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var inOrderArray = [100,150,200]
+    var index=0
+    tree3.root?.traverseInorder() { node in
+      XCTAssertTrue( node.value == inOrderArray[index])
+      index += 1
+    }
+  }
+  
+  func testPreOrderSimple() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var preOrderArray = [100,50,150]
+    var index=0
+    tree1.root?.traversePreorder() { node in
+      XCTAssertTrue( node.value == preOrderArray[index])
+      index += 1
+    }
+  }
+  
+  func testPreOrderSimpleAllLeft() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var preOrderArray = [100,50,25]
+    var index=0
+    tree2.root?.traversePreorder() { node in
+      XCTAssertTrue( node.value == preOrderArray[index])
+      index += 1
+    }
+  }
+  
+  func testPreOrderSimpleAllRight() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var preOrderArray = [100,150,200]
+    var index=0
+    tree3.root?.traversePreorder() { node in
+      XCTAssertTrue( node.value == preOrderArray[index])
+      index += 1
+    }
+  }
+  
+  
+  func testPostOrderSimple() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var postOrderArray = [50,150,100]
+    var index=0
+    tree1.root?.traversePostorder() { node in
+      XCTAssertTrue( node.value == postOrderArray[index])
+      index += 1
+    }
+  }
+  
+  func testPostOrderSimpleAllLeft() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var postOrderArray = [25,50,100]
+    var index=0
+    tree2.root?.traversePostorder() { node in
+      XCTAssertTrue( node.value == postOrderArray[index])
+      index += 1
+    }
+  }
+  
+  func testPostOrderSimpleAllRight() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    var postOrderArray = [200,150,100]
+    var index=0
+    tree3.root?.traversePostorder() { node in
+      XCTAssertTrue( node.value == postOrderArray[index])
+      index += 1
+    }
+  }
+  
+  func testLCA1() {
+    let dataSets: [LCATestDataType] = [lcaDataSet1, lcaDataSet2, lcaDataSet3]
+    dataSets.forEach { lcaDataSet in
+      
+      XCTAssert(lcaDataSet.root.commonAnsestorOf(node: lcaDataSet.node1, andNode: lcaDataSet.node2) == lcaDataSet.lca)
+      XCTAssert(lcaDataSet.root.commonAnsestorOf(node: lcaDataSet.node1, andNode: lcaDataSet.node2) == lcaDataSet.root.commonAnsestorOf(node: lcaDataSet.node2, andNode: lcaDataSet.node1))
+      
+    }
+  }
 
-    private var tree1: BTree<Int> {
-        let rootNode = BTreeNode<Int>(value: 100)
-        let left1 = BTreeNode<Int>(value: 50)
-        let right1 = BTreeNode<Int>(value: 150)
-        rootNode.left = left1
-        rootNode.right = right1
-        let treeNode = BTree<Int>()
-        treeNode.root = rootNode
-        return treeNode
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    private var tree2: BTree<Int> {
-        let rootNode = BTreeNode<Int>(value: 100)
-        let left1 = BTreeNode<Int>(value: 50)
-        let left2 = BTreeNode<Int>(value: 25)
-        rootNode.left = left1
-        left1.left = left2
-        let treeNode = BTree<Int>()
-        treeNode.root = rootNode
-        return treeNode
-    }
-    private var tree3: BTree<Int> {
-        let rootNode = BTreeNode<Int>(value: 100)
-        let right1 = BTreeNode<Int>(value: 150)
-        let right2 = BTreeNode<Int>(value: 200)
-        rootNode.right = right1
-        right1.right = right2
-        let treeNode = BTree<Int>()
-        treeNode.root = rootNode
-        return treeNode
-    }
-    
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testInOrderSimple() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var inOrderArray = [50,100,150]
-        var index=0
-        tree1.root?.traverseInorder() { value in
-            XCTAssertTrue( value == inOrderArray[index])
-            index += 1
-        }
-    }
-    
-    func testInOrderSimpleAllLeft() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var inOrderArray = [25,50,100]
-        var index=0
-        tree2.root?.traverseInorder() { value in
-            XCTAssertTrue( value == inOrderArray[index])
-            index += 1
-        }
-    }
-    
-    func testInOrderSimpleAllRight() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var inOrderArray = [100,150,200]
-        var index=0
-        tree3.root?.traverseInorder() { value in
-            XCTAssertTrue( value == inOrderArray[index])
-            index += 1
-        }
-    }
-    
-    func testPreOrderSimple() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var preOrderArray = [100,50,150]
-        var index=0
-        tree1.root?.traversePreorder() { value in
-            XCTAssertTrue( value == preOrderArray[index])
-            index += 1
-        }
-    }
-    
-    func testPreOrderSimpleAllLeft() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var preOrderArray = [100,50,25]
-        var index=0
-        tree2.root?.traversePreorder() { value in
-            XCTAssertTrue( value == preOrderArray[index])
-            index += 1
-        }
-    }
-    
-    func testPreOrderSimpleAllRight() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var preOrderArray = [100,150,200]
-        var index=0
-        tree3.root?.traversePreorder() { value in
-            XCTAssertTrue( value == preOrderArray[index])
-            index += 1
-        }
-    }
-    
-    
-    func testPostOrderSimple() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var postOrderArray = [50,150,100]
-        var index=0
-        tree1.root?.traversePostorder() { value in
-            XCTAssertTrue( value == postOrderArray[index])
-            index += 1
-        }
-    }
-    
-    func testPostOrderSimpleAllLeft() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var postOrderArray = [25,50,100]
-        var index=0
-        tree2.root?.traversePostorder() { value in
-            XCTAssertTrue( value == postOrderArray[index])
-            index += 1
-        }
-    }
-    
-    func testPostOrderSimpleAllRight() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        var postOrderArray = [200,150,100]
-        var index=0
-        tree3.root?.traversePostorder() { value in
-            XCTAssertTrue( value == postOrderArray[index])
-            index += 1
-        }
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  }
 }
--- Algos/Algos/Tree.swift
+++ Algos/Algos/Tree.swift
@@ -6,11 +6,29 @@
 //  Copyright © 2019 Personal. All rights reserved.
 //
 
-class BTree<T> {
+
+class BTree<T: Comparable> {
   var root: BTreeNode<T>?
+
+  func commonAnsestorOf(node node1:BTreeNode<T>?, andNode node2:BTreeNode<T>?) -> BTreeNode<T>? {
+    guard let root = root else { return nil }
+    guard let node1 = node1 else { return node2 }
+    guard let node2 = node2 else { return node1 }
+    return BTree<T>.findLCA(root: root, node1: node1, node2: node2)
+  }
+
+  private static func findLCA(root: BTreeNode<T>, node1: BTreeNode<T>, node2: BTreeNode<T>) -> BTreeNode<T>? {
+    return root.findLCA(node1: node1, node2: node2)
+  }
 }
 
-class BTreeNode<T> {
+extension BTree: Equatable {
+  static func == (lhs: BTree<T>, rhs: BTree<T>) -> Bool {
+    return lhs.root == rhs.root
+  }
+}
+
+class BTreeNode<T: Comparable> {
   let value: T
   var left: BTreeNode<T>?
   var right: BTreeNode<T>?
@@ -18,20 +36,38 @@ class BTreeNode<T> {
     self.value = value
   }
 
-  func traverseInorder(completion: ((T)->())? ) {
+  typealias Compeltion = (( BTreeNode<T>)->())
+
+  func traverseInorder(completion: Compeltion? ) {
     left?.traverseInorder(completion: completion)
-    completion?(value)
+    completion?(self)
     right?.traverseInorder(completion: completion)
   }
-  func traversePreorder(completion: ((T)->())? ) {
-    completion?(value)
+  func traversePreorder(completion: Compeltion? ) {
+    completion?(self)
     left?.traversePreorder(completion: completion)
     right?.traversePreorder(completion: completion)
   }
-  func traversePostorder(completion: ((T)->())? ) {
+  func traversePostorder(completion: Compeltion? ) {
     left?.traversePostorder(completion: completion)
     right?.traversePostorder(completion: completion)
-    completion?(value)
+    completion?(self)
+  }
+  func findLCA(node1: BTreeNode<T>, node2: BTreeNode<T>) -> BTreeNode<T>? {
+    if node1.value == value || node2.value == value {
+      return self
+    }
+    let leftLca = left?.findLCA(node1: node1, node2: node2)
+    let rightLca = right?.findLCA(node1: node1, node2: node2)
+    if leftLca != nil && rightLca != nil {
+      return self
+    }
+    return leftLca == nil ? rightLca : leftLca
   }
 }
 
+extension BTreeNode: Equatable {
+  static func == (lhs: BTreeNode<T>, rhs: BTreeNode<T>) -> Bool {
+    return (lhs.value == rhs.value) && (lhs.left == rhs.left) && (lhs.right == rhs.right)
+  }
+}
